f56eb2a0c9f52d9eb89d74ba3582952f17811cbb
Martin Wigeborn <martin.wigeborn@gmail.com>
2023-02-24 13:12:51 +0100

    new id

--- src/App.js
+++ src/App.js
@@ -27,6 +27,11 @@ const App = () => {
       title: 'mens',
       imageUrl: 'https://i.ibb.co/R70vBrQ/men.png',
     },
+    {
+      id: 6,
+      title: 'menss',
+      imageUrl: 'https://i.ibb.co/R70vBrQ/men.png',
+    },
   ];
 
   return <Directory categories={categories} />;
